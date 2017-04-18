e639b94654add59644437562f220b86353ee02b9
Adam Smith <adam@akuseru.io>
2017-04-17 21:02:49 -0400

    update event creation to be a method inside ApplicationDbContext

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,149 +11,120 @@ using Microsoft.EntityFrameworkCore;
 
 namespace cal.Controllers
 {
-    public class EventController : Controller
-    {
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ApplicationDbContext _context;
+	public class EventController : Controller
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly ApplicationDbContext _context;
 
-        public EventController(
-            SignInManager<ApplicationUser> signInManager,
-            UserManager<ApplicationUser> userManager,
-            ApplicationDbContext context
-        )
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _context = context;
-        }
-        public async Task<IActionResult> Create(string week, Guid roomId)
-        {
-            var user = await GetCurrentUserAsync();
-            if(user == null)
-                return Redirect("/");
-            else
-            {
-                DateTime WeekOf;
-                if(String.IsNullOrEmpty(week))
-                    WeekOf = DateTime.Today;
-                else
-                    WeekOf = DateTime.Parse(week);
-                
-                var currentDayOfWeek = (int) WeekOf.DayOfWeek;
-                var sunday = WeekOf.AddDays(-currentDayOfWeek);
-                var events = (roomId == null) ? new List<RoomReservation>() : await _context.RoomReservation.Where(i => i.Room.ID == roomId && i.Start > sunday && i.Start < sunday.AddDays(7)).ToListAsync();
-                return View(new CreateViewModel{
-                    Sunday = sunday,
-                    RoomId = roomId,
-                    Rooms = await _context.Rooms.ToListAsync(),
-                    Events = events,
-                });
-            }
-        }
+		public EventController(
+		    SignInManager<ApplicationUser> signInManager,
+		    UserManager<ApplicationUser> userManager,
+		    ApplicationDbContext context
+		)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+			_context = context;
+		}
+		public async Task<IActionResult> Create(string week, Guid roomId)
+		{
+			var user = await GetCurrentUserAsync();
+			if (user == null)
+				return Redirect("/");
+			else
+			{
+				DateTime WeekOf;
+				if (String.IsNullOrEmpty(week))
+					WeekOf = DateTime.Today;
+				else
+					WeekOf = DateTime.Parse(week);
 
-        public async Task<IActionResult> EventPeople(DateTime day, Guid RoomId, List<string> guests)
-        {
-            var user = await GetCurrentUserAsync();
-            if(user == null)
-                return Redirect("/");
-            var r = await _context.Rooms.FirstOrDefaultAsync(i => i.ID == RoomId);
-            if(r == null)
-                return Redirect("/Event/Create?danger=Room Not Found");
-          
-            if(guests.Contains(user.Id))
-                guests.Remove(user.Id);
-            var gl = new List<ApplicationUser>
-            {
-                user
-            };
-            var events = new List<CalendarEvent>();
-            foreach(var s in guests)
-            {
-                var toview = await _userManager.Users.FirstOrDefaultAsync(i => i.Id == s);
-                gl.Add(toview);
-                if(await _context.CanView(user, toview))
-                {
-                    events.AddRange(await _context.LoadUserEvents(toview, user, day, true));
-                }
-            }
-            return View(new CreateViewModel{
-                Sunday = day,
-                Room = r,
-                Guests = gl,
-                UserEvents = events,
-            });
-        }
+				var currentDayOfWeek = (int)WeekOf.DayOfWeek;
+				var sunday = WeekOf.AddDays(-currentDayOfWeek);
+				var events = (roomId == null) ? new List<RoomReservation>() : await _context.RoomReservation.Where(i => i.Room.ID == roomId && i.Start > sunday && i.Start < sunday.AddDays(7)).ToListAsync();
+				return View(new CreateViewModel
+				{
+					Sunday = sunday,
+					RoomId = roomId,
+					Rooms = await _context.Rooms.ToListAsync(),
+					Events = events,
+				});
+			}
+		}
 
-        public async Task<IActionResult> CreateFinal(DateTime day, Guid RoomId, List<string> guests, string start, string end)
-        {
-            var user = await GetCurrentUserAsync();
-            if(user == null)
-                return Redirect("/");
-            
-            var r = await _context.Rooms.FirstOrDefaultAsync(i => i.ID == RoomId);
-            if(r == null)
-                return Redirect("/Event/Create?danger=Room Not Found");
+		public async Task<IActionResult> EventPeople(DateTime day, Guid RoomId, List<string> guests)
+		{
+			var user = await GetCurrentUserAsync();
+			if (user == null)
+				return Redirect("/");
+			var r = await _context.Rooms.FirstOrDefaultAsync(i => i.ID == RoomId);
+			if (r == null)
+				return Redirect("/Event/Create?danger=Room Not Found");
 
-            var eventStart = day;
-            var eventEnd = day;
-            var temp = start.Split(':').Select(i => int.Parse(i)).ToList();
-            eventStart = eventStart.AddHours(temp[0]);
-            eventStart = eventStart.AddMinutes(temp[1]);
+			if (guests.Contains(user.Id))
+				guests.Remove(user.Id);
+			var gl = new List<ApplicationUser>
+	    {
+		user
+	    };
+			var events = new List<CalendarEvent>();
+			foreach (var s in guests)
+			{
+				var toview = await _userManager.Users.FirstOrDefaultAsync(i => i.Id == s);
+				gl.Add(toview);
+				if (await _context.CanView(user, toview))
+				{
+					events.AddRange(await _context.LoadUserEvents(toview, user, day, true));
+				}
+			}
+			return View(new CreateViewModel
+			{
+				Sunday = day,
+				Room = r,
+				Guests = gl,
+				UserEvents = events,
+			});
+		}
 
-            temp = end.Split(':').Select(i => int.Parse(i)).ToList();
-            eventEnd = eventEnd.AddHours(temp[0]);
-            eventEnd = eventEnd.AddHours(temp[1]);
+		public async Task<IActionResult> CreateFinal(DateTime day, Guid RoomId, List<string> guests, string start, string end)
+		{
+			var user = await GetCurrentUserAsync();
+			if (user == null)
+				return Redirect("/");
 
-            var gl = new List<ApplicationUser>();
-            foreach(var s in guests)
-            {
-                var toview = await _userManager.Users.FirstOrDefaultAsync(i => i.Id == s);
-                gl.Add(toview);
-            }
+			var r = await _context.Rooms.FirstOrDefaultAsync(i => i.ID == RoomId);
+			if (r == null)
+				return Redirect("/Event/Create?danger=Room Not Found");
 
-            var newEvent = new RoomReservation{
-                ID = new Guid(),
-                Room = r,
-                Owner = user,
-                Start = eventStart,
-                End = eventEnd,
-                Participants = gl,
-                ReservationType = ReservationType.Regular
-            };
-            _context.RoomReservation.Add(newEvent);
+			var eventStart = day;
+			var eventEnd = day;
+			var temp = start.Split(':').Select(i => int.Parse(i)).ToList();
+			eventStart = eventStart.AddHours(temp[0]);
+			eventStart = eventStart.AddMinutes(temp[1]);
 
-            var calEvent = new CalendarEvent{
-                Id = new Guid(),
-                Event = newEvent,
-                User = user,
-                Start = eventStart,
-                End = eventEnd,
-                ReservationType = ReservationType.Regular
-            };
-            _context.Events.Add(calEvent);
-            foreach(var g in gl)
-            {
-                if(g.Id == user.Id) continue;
-                calEvent = new CalendarEvent{
-                    Id = new Guid(),
-                    Event = newEvent,
-                    User = g,
-                    Start = eventStart,
-                    End = eventEnd,
-                    ReservationType = ReservationType.Regular
-                };
-                _context.Events.Add(calEvent);
-            }
+			temp = end.Split(':').Select(i => int.Parse(i)).ToList();
+			eventEnd = eventEnd.AddHours(temp[0]);
+			eventEnd = eventEnd.AddHours(temp[1]);
 
-            _context.SaveChanges();
+			var gl = new List<ApplicationUser>();
+			foreach (var s in guests)
+			{
+				var toview = await _userManager.Users.FirstOrDefaultAsync(i => i.Id == s);
+				gl.Add(toview);
+			}
 
-            return Redirect("/?msg=Event Created!");   
-        }
-        
-        private Task<ApplicationUser> GetCurrentUserAsync()
-        {
-            return _userManager.GetUserAsync(HttpContext.User);
-        }
-    }
+			var success = await _context.CreateEvent(r, user, gl, eventStart, eventEnd);
+
+			if (!success.Item1)
+				return Redirect("/Event/EventPeople?Room=" + r.ID + "&day=" + string.Format("{0:yyyy-M-d dddd}", day) + "&danger="+success.Item2);
+
+			return Redirect("/?msg=Event Created!");
+		}
+
+		private Task<ApplicationUser> GetCurrentUserAsync()
+		{
+			return _userManager.GetUserAsync(HttpContext.User);
+		}
+	}
 }
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,65 +8,126 @@ using System.Threading.Tasks;
 
 namespace cal.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
 
-        public DbSet<Room> Rooms {get;set;}
-        public DbSet<RoomReservation> RoomReservation{get;set;}
-        public DbSet<CalendarEvent> Events{get;set;}
-        public DbSet<Viewers> Viewers{get;set;}
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<RoomReservation> RoomReservation { get; set; }
+		public DbSet<CalendarEvent> Events { get; set; }
+		public DbSet<Viewers> Viewers { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		    : base(options)
+		{
+		}
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
 
 
 
-       /// <summary>
-       /// Loads a users events for a given week.
-       /// </summary>
-       /// <param name="ForUser">The user who we are getting events for</param>
-       /// <param name="Me">The current user who is requesting the data</param>
-       /// <param name="week">any day for the current week.</param>
-       /// <param name="SingleDay">if true only load up events for a single day</param>
-       /// <returns>a list of events. If teh user is not allowed to view the forusers events we return an empty set.</returns>
-        public async Task<List<CalendarEvent>> LoadUserEvents(ApplicationUser ForUser, ApplicationUser Me, DateTime week, bool SingleDay = false)
-        {
-            //reset to sunday to get the events for the week.
-            if(week.DayOfWeek != DayOfWeek.Sunday)
-            {
-                week = week.AddDays(-(int)week.DayOfWeek);
-            }
+		/// <summary>
+		/// Loads a users events for a given week.
+		/// </summary>
+		/// <param name="ForUser">The user who we are getting events for</param>
+		/// <param name="Me">The current user who is requesting the data</param>
+		/// <param name="week">any day for the current week.</param>
+		/// <param name="SingleDay">if true only load up events for a single day</param>
+		/// <returns>a list of events. If teh user is not allowed to view the forusers events we return an empty set.</returns>
+		public async Task<List<CalendarEvent>> LoadUserEvents(ApplicationUser ForUser, ApplicationUser Me, DateTime week, bool SingleDay = false)
+		{
+			//reset to sunday to get the events for the week.
+			if (week.DayOfWeek != DayOfWeek.Sunday)
+			{
+				week = week.AddDays(-(int)week.DayOfWeek);
+			}
 
-            //
-            if( ! await CanView(Me, ForUser))
-            {
-                return new List<CalendarEvent>();
-            }
-            DateTime End = week.AddDays(7);
-            if(SingleDay)
-            {
-                End = week.AddDays(1);
-            }
+			//
+			if (!await CanView(Me, ForUser))
+			{
+				return new List<CalendarEvent>();
+			}
+			DateTime End = week.AddDays(7);
+			if (SingleDay)
+			{
+				End = week.AddDays(1);
+			}
 
-            return await Events.Where(i => i.User == ForUser && i.Start > week && i.End < End).ToListAsync();
-        }
-        public async Task<bool> CanView(ApplicationUser me, ApplicationUser UserMeWantsToView)
-        {
-            if(me == UserMeWantsToView)
-                return true;
-            if(me.Role == UserRole.AdminAssistant || me.Role == UserRole.Administrator)
-                return true;
-            var found = await Viewers.FirstOrDefaultAsync(i => i.Owner == UserMeWantsToView && i.Viewer == me);
-            
-            return found != default(Viewers);
-        }
+			return await Events.Where(i => i.User == ForUser && i.Start > week && i.End < End).ToListAsync();
+		}
+		/// <summary>
+		/// Checks to see if a specific user can view someone elses calendar.
+		/// </summary>
+		/// <returns>The view.</returns>
+		/// <param name="me">Me.</param>
+		/// <param name="UserMeWantsToView">User me wants to view.</param>
+		public async Task<bool> CanView(ApplicationUser me, ApplicationUser UserMeWantsToView)
+		{
+			if (me == UserMeWantsToView)
+				return true;
+			if (me.Role == UserRole.AdminAssistant || me.Role == UserRole.Administrator)
+				return true;
+			var found = await Viewers.FirstOrDefaultAsync(i => i.Owner == UserMeWantsToView && i.Viewer == me);
 
-    }
+			return found != default(Viewers);
+		}
+
+		/// <summary>
+		/// create an event.
+		/// </summary>
+		/// <returns>The event.</returns>
+		/// <param name="room">Room.</param>
+		/// <param name="user">Creator.</param>
+		/// <param name="participants">Participants.</param>
+		/// <param name="eventStart">Start.</param>
+		/// <param name="eventEnd">End.</param>
+		public async Task<Tuple<bool, string>> CreateEvent(Room room, ApplicationUser user, List<ApplicationUser> participants, DateTime eventStart, DateTime eventEnd)
+		{
+			if (eventStart <= eventEnd)
+				return new Tuple<bool, string>(false, "End time must be after Start time");
+			var newEvent = new RoomReservation
+			{
+				ID = new Guid(),
+				Room = room,
+				Owner = user,
+				Start = eventStart,
+				End = eventEnd,
+				Participants = participants,
+				ReservationType = ReservationType.Regular
+			};
+			RoomReservation.Add(newEvent);
+
+			var calEvent = new CalendarEvent
+			{
+				Id = new Guid(),
+				Event = newEvent,
+				User = user,
+				Start = eventStart,
+				End = eventEnd,
+				ReservationType = ReservationType.Regular
+			};
+			Events.Add(calEvent);
+			foreach (var g in participants)
+			{
+				if (g.Id == user.Id) continue;
+				calEvent = new CalendarEvent
+				{
+					Id = new Guid(),
+					Event = newEvent,
+					User = g,
+					Start = eventStart,
+					End = eventEnd,
+					ReservationType = ReservationType.Regular
+				};
+				Events.Add(calEvent);
+			}
+			await SaveChangesAsync();
+
+			return new Tuple<bool, string>(true, "");
+
+		}
+
+	}
 }
